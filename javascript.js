22cef71f6072e9c87da675f78d71a15aedd5b0eb
kavyapc <36132470+kavyapc@users.noreply.github.com>
2026-02-24 08:57:06 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
- 
-      const button = document.getElementById("magic-button");
+<script>
+const button = document.getElementById("magic-button");
 const box = document.getElementById("message-box");
 // 2. Listen for a click on the button
 button.addEventListener("click", function() {
 // 3. When clicked, run this function:
 changeMessage();
 });
-        function changeMessage() {
+ function changeMessage() {
 // Change the text inside the div
 box.textContent = " You clicked! I have been changed by JavaScript!";
 // Change the background color using inline CSS
@@ -14,4 +14,5 @@ box.style.backgroundColor = "lightgreen";
 // Add a CSS class (defined in style.css)
 box.classList.add("success-box");
 }
-  
+</script>
+ 
