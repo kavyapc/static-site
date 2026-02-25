a81d001198fd9c058b610bf1188596e4d25b4dbb
kavyapc <36132470+kavyapc@users.noreply.github.com>
2026-02-24 22:45:46 -0700

    Wrap JavaScript code in script tags
    Added opening and closing script tags to the JavaScript file.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-
+<script>
+ 
 const button = document.getElementById("magic-button");
 const box = document.getElementById("message-box");
 // 2. Listen for a click on the button
@@ -14,5 +15,5 @@ box.style.backgroundColor = "lightgreen";
 // Add a CSS class (defined in style.css)
 box.classList.add("success-box");
 }
-
+</script>
  
